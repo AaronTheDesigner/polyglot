36b5e28acf3e1efaa75f064f1c8da16548e85934
Aaron Toliver <flux.charlotte@gmail.com>
2021-01-06 15:26:11 -0500

    average calculations

--- JavaScript_BackToBasics/math.js
+++ JavaScript_BackToBasics/math.js
@@ -48,6 +48,7 @@ const mathMax = Math.max(2, 33, 78, 918, 17); // find maximum
 const mathRandom = Math.random();
 const mathControlRandom = Math.random() * 20 + 1; // random decimal between 0 and 19 + 1, which is 20. use floor to commit to whole numbers. 
 const mathFloorRandom = Math.floor(Math.random() * 20 + 1);
+const mathFixed = 12.8273847.toFixed(2);
 
 
 // Challenge 1 
@@ -56,12 +57,14 @@ const mathFloorRandom = Math.floor(Math.random() * 20 + 1);
 
 const transactions = [93473,7483,3748,7383,7483,7483,72819,8749,7489,73849,982,784,928];
 
+// reduce
 const basicAverage = arr => {
     const sum = arr.reduce((acc, cur) => acc + cur);
     const average = sum/arr.length;
-    return average;
+    return average.toFixed(2);
 }
 
+// while loop
 const whileAverage = arr => {
     let i = 0;
     let sum = 0; 
@@ -69,7 +72,84 @@ const whileAverage = arr => {
     while (i < len) {
         sum = sum + arr[i++];
     }
-    return sum / len;
+    let average = sum / len;
+    return average.toFixed(2)
 }
 
-console.log(whileAverage(transactions));
+//console.log(whileAverage(transactions));
+const astronomyList = [
+    {
+        item: 'star chart',
+        found: true,
+        price: 108.22,
+    },
+    {
+        item: 'telescope',
+        found: false,
+        price: 409.28,
+    },
+    {
+        item: 'astrolabe',
+        found: true,
+        price: 609.05,
+    },
+    {
+        item: 'map',
+        found: false,
+        price: 122.07,
+    },
+    {
+        item: 'compass',
+        found: true,
+        price: 232.00,
+    },
+    {
+        item: 'Gallileo Writings',
+        found: false,
+        price: 30.77,
+    },
+    {
+        item: 'Coupernicus Writings',
+        found: true,
+        price: 25.99,
+    }
+
+];
+
+// imperative loop
+// loop through all objects, grab all prices, calculate averages
+
+const imperAverage = arr => {
+    // initialize sum of prices
+    let priceSum = 0;
+    // initialize how many items have been found
+    // let itemsFound = 0;
+    // initialize the length of the array
+    const len = arr.length;
+
+    for (let i = 0;  i < len; i++) {
+        // grab each item from the list
+        let item = arr[i];
+        priceSum = item.price + priceSum;
+    }
+    let average = priceSum / len;
+    return average.toFixed(2);
+}
+
+//console.log(imperAverage(astronomyList));
+
+const averageForIn = arr => {
+    let priceSum = 0;
+    let itemCount = arr.length;
+
+    // for of loop meant for arrays, even with objects within them
+    // fo in loop does not work here. 
+    for (let item of arr) {
+        priceSum = item.price + priceSum;
+    }
+
+    let average = priceSum / itemCount;
+    return average.toFixed(2);
+}
+
+console.log(averageForIn(astronomyList));
